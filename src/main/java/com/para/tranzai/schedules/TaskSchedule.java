5b959336e10ebc11a05ae8819c555b12e4cfa38f
lichengkun <lichengkun@csrd.cn>
2021-12-30 14:38:41 +0800

    ADD:加入机器人权限认证

--- src/main/java/com/para/tranzai/schedules/TaskSchedule.java
+++ src/main/java/com/para/tranzai/schedules/TaskSchedule.java
@@ -3,7 +3,6 @@ package com.para.tranzai.schedules;
 import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.util.StrUtil;
 import com.para.tranzai.mirai.exception.NoGroupFoundException;
-import com.para.tranzai.mirai.server.MiraiService;
 import com.para.tranzai.para.entity.Page;
 import com.para.tranzai.para.entity.PageResult;
 import com.para.tranzai.para.entity.Task;
@@ -11,6 +10,9 @@ import com.para.tranzai.para.server.ParaService;
 import com.para.tranzai.properties.TranzaiProperties;
 import com.para.tranzai.tools.GlobalVariable;
 import lombok.extern.slf4j.Slf4j;
+import net.mamoe.mirai.Bot;
+import net.mamoe.mirai.contact.Group;
+import net.mamoe.mirai.contact.MemberPermission;
 import net.mamoe.mirai.message.data.AtAll;
 import net.mamoe.mirai.message.data.MessageChain;
 import net.mamoe.mirai.message.data.MessageChainBuilder;
@@ -26,14 +28,14 @@ import java.util.Collection;
 public class TaskSchedule {
 
     private final TranzaiProperties properties;
-
     private final ParaService paraService;
-    private final MiraiService miraiService;
 
-    public TaskSchedule(TranzaiProperties properties, ParaService paraService, MiraiService miraiService) {
+    private final Bot bot;
+
+    public TaskSchedule(TranzaiProperties properties, ParaService paraService, Bot bot) {
         this.properties = properties;
         this.paraService = paraService;
-        this.miraiService = miraiService;
+        this.bot = bot;
     }
 
     /**
@@ -49,9 +51,12 @@ public class TaskSchedule {
             Collection<Task> diffedObjs = CollUtil.disjunction(pageResult.getResults(), GlobalVariable.taskCaches.getResults());
             //执行q群推送...
             if (CollUtil.isNotEmpty(diffedObjs)) {
-                for (Long group : properties.getMiraiBotConfig().getGroups()) {
+                for (Long id : properties.getMiraiBotConfig().getGroups()) {
                     try {
-                        miraiService.sendGroupMessage(group, buildMessage(diffedObjs));
+                        Group group = bot.getGroup(id);
+                        if (group != null) {
+                            group.sendMessage(buildMessage(group, diffedObjs));
+                        }
                     } catch (NoGroupFoundException e) {
                         e.printStackTrace();
                     }
@@ -60,9 +65,12 @@ public class TaskSchedule {
         }
     }
 
-    private MessageChain buildMessage(Collection<Task> diffObj) {
-        MessageChainBuilder builder = new MessageChainBuilder()
-                .append(AtAll.INSTANCE);
+    private MessageChain buildMessage(Group group, Collection<Task> diffObj) {
+        MessageChainBuilder builder = new MessageChainBuilder();
+        //如果机器人是管理员，则加上@全体操作.
+        if (group.getBotPermission().getLevel() == MemberPermission.ADMINISTRATOR.getLevel()) {
+            builder.append(AtAll.INSTANCE);
+        }
         for (Task task : diffObj) {
             String format = StrUtil.format("{} 发布了新任务：[{}]", task.getUser().getNickname(), task.getTitle());
             builder.append(format);
