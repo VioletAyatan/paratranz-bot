bd8535da6808408c0b2a10f0901525a6139619ac
lichengkun <2329340232@qq.com>
2021-12-31 22:11:54 +0800

    更新脚本

--- setup/scripts/start.sh
+++ setup/scripts/start.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+
 dateTime=$(date +%Y%m%d%H%M%S)
-logFile=/opt/server/logs
-if [ -e /opt/server/log.out ]; then
-  mv log.out $logFile/log.out.$dateTime
+
+logFile=/opt/tranzai/logs
+
+if [ -e /opt/tranzai/log.out ]; then
+  mv log.out $logFile/log.out."$dateTime"
 fi
-nohup java -Xms500m -Xmx500m -Xmn250m -Xss256k -server -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -jar /opt/server/tranzai.jar >log.out &
+
+nohup java -Xms500m -Xmx500m -Xmn250m -Xss256k -server -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -jar /opt/tranzai/tranzai.jar >log.out &
--- setup/scripts/stop.sh
+++ setup/scripts/stop.sh
@@ -2,10 +2,10 @@
 ##echo "kill process xljc-0.0.1-SNAPSHOT.jar"
 PID=$(ps -ef | grep tranzai.jar | grep -v grep | awk '{print $2}')
 if [ -z $PID ]; then
-  echo "process server.jar not exist"
+  echo "process tranzai.jar not exist"
   exit
 else
   echo "process id: $PID"
   kill -9 ${PID}
-  echo "process server.jar killed"
+  echo "process tranzai.jar killed"
 fi
