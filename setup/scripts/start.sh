d8dcce2e4be3a9d17e6388f1ad79b6fa13881ccc
lichengkun <lichengkun@csrd.cn>
2021-12-31 11:20:51 +0800

    修正脚本

--- setup/scripts/start.sh
+++ setup/scripts/start.sh
@@ -4,4 +4,4 @@ logFile=/opt/server/logs
 if [ -e /opt/server/log.out ]; then
   mv log.out $logFile/log.out.$dateTime
 fi
-nohup java -Xms500m -Xmx500m -Xmn250m -Xss256k -server -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -jar /opt/server/server.jar >log.out &
+nohup java -Xms500m -Xmx500m -Xmn250m -Xss256k -server -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -jar /opt/server/tranzai.jar >log.out &
--- setup/scripts/stop.sh
+++ setup/scripts/stop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ##echo "kill process xljc-0.0.1-SNAPSHOT.jar"
-PID=$(ps -ef | grep server.jar | grep -v grep | awk '{print $2}')
+PID=$(ps -ef | grep tranzai.jar | grep -v grep | awk '{print $2}')
 if [ -z $PID ]; then
   echo "process server.jar not exist"
   exit
