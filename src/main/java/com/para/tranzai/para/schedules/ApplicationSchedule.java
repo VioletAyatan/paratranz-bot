8b1cb6d3058ba0d059fa0077c12f64f54a7edf6d
lichengkun <lichengkun@csrd.cn>
2022-03-28 16:58:42 +0800

    CHANGE：屏蔽关于推送至q群的信息中的英语等级与游戏时间

--- src/main/java/com/para/tranzai/para/schedules/ApplicationSchedule.java
+++ src/main/java/com/para/tranzai/para/schedules/ApplicationSchedule.java
@@ -81,10 +81,10 @@ public class ApplicationSchedule {
         for (Application application : collect) {
             Application.UserDTO user = application.getUser();
             builder.append(StrUtil.format("{} 申请加入项目组，请及时审核。\n", user.getUsername()));
-            builder.append(StrUtil.format("游戏时间 {}，英语等级：{}\n",
-                    application.getDetail().getGameTime(),
-                    application.getDetail().getEnglish().getLevel())
-            );
+//            builder.append(StrUtil.format("游戏时间 {}，英语等级：{}\n",
+//                    application.getDetail().getGameTime(),
+//                    application.getDetail().getEnglish().getLevel())
+//            );
             if (StrUtil.isNotBlank(application.getContent())) {
                 builder.append(StrUtil.format(application.getContent()));
             }
