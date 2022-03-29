4bbbbe5ca888de579d6dcd39777fcba0c6a9b50a
lichengkun <lichengkun@csrd.cn>
2022-03-29 09:04:20 +0800

    删除无用逻辑

--- src/main/java/com/para/tranzai/tools/GlobalVariable.java
+++
@@ -1,9 +0,0 @@
-package com.para.tranzai.tools;
-
-import com.para.tranzai.para.entity.PageResult;
-import com.para.tranzai.para.entity.data.Task;
-
-public class GlobalVariable {
-
-    public static PageResult<Task> taskCaches = new PageResult<>();
-}
