3dc4272b18e53e107cbc75affb96a508b696da31
lichengkun <lichengkun@csrd.cn>
2022-06-10 16:36:10 +0800

    完善参数校验

--- src/main/java/com/para/tranzai/command/core/CommandProcessorHandler.java
+++ src/main/java/com/para/tranzai/command/core/CommandProcessorHandler.java
@@ -7,6 +7,9 @@ import lombok.extern.slf4j.Slf4j;
 import net.mamoe.mirai.event.events.MessageEvent;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 
 import java.lang.reflect.Type;
 import java.util.List;
@@ -19,23 +22,16 @@ import java.util.function.Predicate;
  */
 @Slf4j
 //@Component
-public class CommandProcessorHandler implements BeanPostProcessor {
+public class CommandProcessorHandler implements BeanPostProcessor, ApplicationContextAware {
+
     /**
      * 参数校验器
      */
-    private final List<Predicate<Type>> predicates = List.of(type -> {
-                if (type instanceof Class) {
-                    return ClassUtil.isAssignable(MessageEvent.class, (Class<?>) type);
-                }
-                return false;
-            },
-            type -> {
-                if (type instanceof Class) {
-                    return ClassUtil.isAssignable(MessageEvent.class, (Class<?>) type);
-                }
-                return false;
-            }
+    private static final List<Predicate<Class<?>>> argumentValidators = List.of(
+            type -> ClassUtil.isAssignable(MessageEvent.class, type),
+            type -> ClassUtil.isAssignable(MessageEvent.class, type)
     );
+    private ApplicationContext applicationContext;
 
     @Override
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
@@ -43,10 +39,11 @@ public class CommandProcessorHandler implements BeanPostProcessor {
         if (hasAnnotation) {
             CommandProcessor annotation = AnnotationUtil.getAnnotation(bean.getClass(), CommandProcessor.class);
             try {
-                checkArgument(annotation, bean, beanName);
+                checkArgument(bean, beanName);
                 registerCommandProcessor(annotation, bean);
-            } catch (Exception e) {
-                throw new RuntimeException(e);
+            } catch (RuntimeException e) {
+                log.error(e.getMessage());
+                SpringApplication.exit(applicationContext);
             }
         }
         return bean;
@@ -56,13 +53,12 @@ public class CommandProcessorHandler implements BeanPostProcessor {
 
     }
 
-    @SuppressWarnings("unchecked")
-    private void checkArgument(CommandProcessor processor, Object bean, String beanName) throws Exception {
+    private void checkArgument(Object bean, String beanName) throws RuntimeException {
         if (ClassUtil.isAssignable(BiConsumer.class, bean.getClass())) {
             Type[] typeArguments = TypeUtil.getTypeArguments(bean.getClass());
             for (int i = 0; i < typeArguments.length; i++) {
-                if (!predicates.get(i).test(typeArguments[i])) {
-                    throw new RuntimeException();
+                if (!argumentValidators.get(i).test((Class<?>) typeArguments[i])) {
+                    throw new RuntimeException("Illegal TypeArgument [" + typeArguments[i] + "]");
                 }
             }
         } else {
@@ -70,4 +66,8 @@ public class CommandProcessorHandler implements BeanPostProcessor {
         }
     }
 
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
 }
