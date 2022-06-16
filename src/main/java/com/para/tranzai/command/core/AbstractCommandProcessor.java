e4118d15a603b115d8f64b5459f08c8e16b720f8
lichengkun <lichengkun@csrd.cn>
2022-06-16 17:19:22 +0800

    添加注释

--- src/main/java/com/para/tranzai/command/core/AbstractCommandProcessor.java
+++ src/main/java/com/para/tranzai/command/core/AbstractCommandProcessor.java
@@ -5,6 +5,11 @@ import net.mamoe.mirai.event.events.MessageEvent;
 import java.util.function.BiConsumer;
 
 public abstract class AbstractCommandProcessor<E extends MessageEvent> implements BiConsumer<E, String[]> {
+    /**
+     * 对接收到的指令进行处理
+     * @param event 触发的消息事件
+     * @param args 指令参数集.
+     */
     @Override
     public abstract void accept(E event, String[] args);
 }
