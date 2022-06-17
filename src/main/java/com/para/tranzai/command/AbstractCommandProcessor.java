f2a672bc0eead5fb77b29627607b39d6eeb02719
Ankol <2329340232@qq.com>
2022-06-17 17:12:36 +0800

    添加模板方法，针对指令调用分为有参调用，无参调用，子类自行重写对应的方法

--- src/main/java/com/para/tranzai/command/AbstractCommandProcessor.java
+++ src/main/java/com/para/tranzai/command/AbstractCommandProcessor.java
@@ -2,6 +2,7 @@ package com.para.tranzai.command;
 
 import net.mamoe.mirai.event.events.MessageEvent;
 
+import java.util.Arrays;
 import java.util.function.BiConsumer;
 
 public abstract class AbstractCommandProcessor<E extends MessageEvent> implements BiConsumer<E, String[]> {
@@ -11,5 +12,26 @@ public abstract class AbstractCommandProcessor<E extends MessageEvent> implement
      * @param args 指令参数集.
      */
     @Override
-    public abstract void accept(E event, String[] args);
+    public void accept(E event, String[] args) {
+        if (args.length > 1) {
+            triggerArgsEvent(event, Arrays.copyOfRange(args, 1, args.length));
+        } else {
+            triggerNoArgs(event);
+        }
+    }
+
+    /**
+     * 无参数调用的情况下触发此方法
+     * @param event 事件对象
+     */
+    protected void triggerNoArgs(E event) {
+
+    }
+
+    /**
+     * 有参数调用的情况下触发此方法
+     */
+    protected void triggerArgsEvent(E event, String[] args) {
+
+    }
 }
