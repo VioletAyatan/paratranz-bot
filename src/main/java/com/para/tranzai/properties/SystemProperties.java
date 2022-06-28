60efa46f1341183b7879b21e3b5c92219c53f672
Ankol <2329340232@qq.com>
2022-06-28 10:11:03 +0800

    :new: 整合系统配置文件

--- src/main/java/com/para/tranzai/properties/SystemProperties.java
+++ src/main/java/com/para/tranzai/properties/SystemProperties.java
@@ -22,4 +22,34 @@ public class SystemProperties {
      * 模块控制，调试用，用以控制系统各模块激活
      */
     private ModuleControl moduleControl;
+
+    @Getter
+    @Setter
+    public static class ModuleControl {
+        /**
+         * 影响系统中有关paraTranz相关服务的启停
+         */
+        private boolean paraActive;
+        /**
+         * 影响系统中有关mirai相关服务的启停
+         */
+        private boolean miraiActive;
+    }
+
+    @Getter
+    @Setter
+    public static class BotConfig {
+        /**
+         * 机器人qq号
+         */
+        private Long qqNumber;
+        /**
+         * 机器人qq密码
+         */
+        private String password;
+        /**
+         * 配置机器人默认转发消息到哪些群组.
+         */
+        private Long[] groups = new Long[0];
+    }
 }
