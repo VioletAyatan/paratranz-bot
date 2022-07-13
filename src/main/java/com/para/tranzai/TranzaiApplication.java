3ac1468128802975305385e83a859039ad25fda2
Ankol <2329340232@qq.com>
2022-07-13 10:10:22 +0800

    :necktie: 添加SpringBoot版本打印

--- src/main/java/com/para/tranzai/TranzaiApplication.java
+++ src/main/java/com/para/tranzai/TranzaiApplication.java
@@ -1,15 +1,19 @@
 package com.para.tranzai;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootVersion;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@Slf4j
 @EnableScheduling
 @SpringBootApplication
 public class TranzaiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TranzaiApplication.class, args);
+        log.info("Started Spring Boot. Version {}", SpringBootVersion.getVersion());
     }
 
 }
