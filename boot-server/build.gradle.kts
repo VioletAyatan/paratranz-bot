7f857ec2addfaa329888b65cb1702fa814df2c16
Ankol <2329340232@qq.com>
2022-11-18 16:30:14 +0800

    :construction: work in progress

---
+++ boot-server/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    java
+    id("org.springframework.boot") version "2.7.5"
+    id("io.freefair.lombok") version "6.5.1"
+    kotlin("jvm") version "1.7.20"
+    id("io.spring.dependency-management") version "1.1.0"
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+    api(platform("net.mamoe:mirai-bom:2.12.3"))
+    api("net.mamoe:mirai-core-api-jvm") {
+        exclude("net.mamoe", "mirai-core-utils")
+    }
+    runtimeOnly("net.mamoe:mirai-core-jvm") {
+        exclude("net.mamoe", "mirai-core-api")
+        exclude("net.mamoe", "mirai-core-utils")
+    }
+    implementation("net.mamoe:mirai-core-utils-jvm")
+    implementation("com.google.code.gson:gson")
+    implementation("cn.hutool:hutool-http:5.8.9")
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
