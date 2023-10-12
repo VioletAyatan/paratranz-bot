4a94eb1b0f49ecd810fb4ae95724b8efcdab3c55
lichengkun <lichengkun@csrd.cn>
2023-10-12 11:23:51 +0800

    :art: 添加颜色表类

---
+++ boot-server/src/main/java/org/paratranz/bot/Colors.java
@@ -0,0 +1,21 @@
+package org.paratranz.bot;
+
+/**
+ * 颜色表
+ * 值使用16进制表示.
+ * @author Ankol
+ */
+public abstract class Colors {
+    /**
+     * 红
+     */
+    public static final int RED = 0xFFFF0000;
+    /**
+     * 蓝
+     */
+    public static final int BLUE = 0xFF0000FF;
+    /**
+     * 绿
+     */
+    public static final int GREEN = 0xFF008000;
+}
