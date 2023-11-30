f7e129feaa5198cbca64255ff2f580ffc10f838c
lichengkun <lichengkun@csrd.cn>
2023-11-30 11:01:00 +0800

    添加web控制器

--- src/main/java/org/paratranz/bot/web/controller/WebController.java
+++ src/main/java/org/paratranz/bot/web/controller/WebController.java
@@ -1,4 +1,4 @@
-package org.paratranz.bot.web;
+package org.paratranz.bot.web.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
