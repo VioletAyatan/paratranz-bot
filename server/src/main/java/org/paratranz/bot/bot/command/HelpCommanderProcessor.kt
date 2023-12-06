6aa8f907c658d0fdaac806de5c9d3a2900354cd1
lichengkun <lichengkun@csrd.cn>
2023-12-06 17:07:25 +0800

    迁移到kotlin

--- server/src/main/java/org/paratranz/bot/bot/command/HelpCommanderProcessor.kt
+++ server/src/main/java/org/paratranz/bot/bot/command/HelpCommanderProcessor.kt
@@ -1,8 +1,8 @@
-package org.paratranz.bot.bot.command;
+package org.paratranz.bot.bot.command
 
-import net.mamoe.mirai.event.events.GroupMessageEvent;
-import org.paratranz.bot.bot.core.GroupMessageCommandProcessor;
-import org.springframework.stereotype.Component;
+import net.mamoe.mirai.event.events.GroupMessageEvent
+import org.paratranz.bot.bot.core.GroupMessageCommandProcessor
+import org.springframework.stereotype.Component
 
 /**
  * /help指令处理器
@@ -10,20 +10,14 @@ import org.springframework.stereotype.Component;
  * @author Ankol
  */
 @Component
-public class HelpCommanderProcessor extends GroupMessageCommandProcessor {
-
-
-    public HelpCommanderProcessor() {
-        super("description", new String[]{"/help", "/帮助"});
-    }
-
+class HelpCommanderProcessor :
+    GroupMessageCommandProcessor("description", arrayOf("/help", "/帮助")) {
     /**
      * 无参数调用的情况下触发此方法
      *
      * @param event 事件对象
      */
-    @Override
-    protected void onNoArgsEvent(GroupMessageEvent event) {
-        super.onNoArgsEvent(event);
+    override fun onNoArgsEvent(event: GroupMessageEvent) {
+        super.onNoArgsEvent(event)
     }
 }
