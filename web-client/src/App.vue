110cac2230d0dcccc2bbcbcc2a127d2604e64760
lichengkun <lichengkun@csrd.cn>
2023-12-07 11:25:41 +0800

    :construction: Work in progress.

--- web-client/src/App.vue
+++ web-client/src/App.vue
@@ -1,31 +1,23 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+</script>
 
 <template>
-	<!-- <div>
-		<a href="https://vitejs.dev" target="_blank">
-			<img src="/vite.svg" class="logo" alt="Vite logo" />
-		</a>
-		<a href="https://vuejs.org/" target="_blank">
-			<img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-		</a>
-	</div>
-	<HelloWorld msg="Vite + Vue" /> -->
-	<div>
-		<RouterView />
-	</div>
+  <div id="app">
+    <div>
+      <RouterView/>
+    </div>
+    <div>
+      <el-form label-width="120">
+        <el-form-item label="Topic">
+          <el-input readonly placeholder="Topic"/>
+        </el-form-item>
+        <el-form-item label="Payload">
+          <el-input type="textarea" readonly placeholder="Payload"/>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
 </template>
 
 <style scoped>
-.logo {
-	height: 6em;
-	padding: 1.5em;
-	will-change: filter;
-	transition: filter 300ms;
-}
-.logo:hover {
-	filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-	filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
