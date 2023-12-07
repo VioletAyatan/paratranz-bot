83df936abc60a4a9ced29685e4f3157180179854
lichengkun <lichengkun@csrd.cn>
2023-12-07 10:53:38 +0800

    初始化bs项目

---
+++ web-client/src/pages/realtime.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+	<div class="page_realtime">
+        
+    
+	</div>
+</template>
+
+<style lang="scss"></style>
---
+++ web-client/vite.config.ts
@@ -0,0 +1,25 @@
+import {defineConfig} from 'vite';
+import vue from '@vitejs/plugin-vue';
+import Pages from 'vite-plugin-pages'; //See: https://github.com/hannoeru/vite-plugin-pages
+import AutoImport from 'unplugin-auto-import/vite'; //See: https://github.com/unplugin/unplugin-auto-import
+import Components from 'unplugin-vue-components/vite'; //See: https://github.com/unplugin/unplugin-vue-components
+import ElementPlus from 'unplugin-element-plus/vite'; //See: https://element-plus.org/zh-CN/guide/quickstart.htm
+import {ElementPlusResolver} from 'unplugin-vue-components/resolvers';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [
+		vue(),
+		AutoImport({
+			imports: ['vue', 'vue-router', 'pinia'],
+			dts: true,
+			resolvers: [ElementPlusResolver()],
+		}),
+		Components({
+			dts: true,
+			resolvers: [ElementPlusResolver()],
+		}),
+		ElementPlus({}),
+		Pages(),
+	],
+});
