3e1cbfb4735cdf2400b0c71976cd77aa16a9eef3
lichengkun <lichengkun@csrd.cn>
2023-12-11 09:24:26 +0800

    :construction: Work in progress.

--- web-client/src/App.vue
+++ web-client/src/App.vue
@@ -3,19 +3,12 @@
 
 <template>
   <div id="app">
-    <div>
-      <RouterView/>
-    </div>
-    <div>
-      <el-form label-width="120">
-        <el-form-item label="Topic">
-          <el-input readonly placeholder="Topic"/>
-        </el-form-item>
-        <el-form-item label="Payload">
-          <el-input type="textarea" readonly placeholder="Payload"/>
-        </el-form-item>
-      </el-form>
-    </div>
+    <el-container>
+      <el-header></el-header>
+      <el-main>
+        <router-view/>
+      </el-main>
+    </el-container>
   </div>
 </template>
 
---
+++ web-client/src/pages/dependencies.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="page_dependencies">
+    <el-table>
+      <el-table-column prop="id" label="id"/>
+    </el-table>
+  </div>
+</template>
+
+<style lang="scss"></style>
