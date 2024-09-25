1e7ba7f88913853609bda0b2b981edee93a8adcd
lichengkun <lichengkun@csrd.cn>
2024-09-25 16:00:51 +0800

    commit

--- server/src/main/java/org/paratranz/bot/tools/JacksonUtil.kt
+++ server/src/main/java/org/paratranz/bot/tools/JacksonUtil.kt
@@ -1,71 +1,120 @@
-package org.paratranz.bot.tools;
+package org.paratranz.bot.tools
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonParser;
-import com.google.gson.JsonSyntaxException;
-import com.google.gson.reflect.TypeToken;
-
-import java.lang.reflect.Type;
+import com.fasterxml.jackson.core.JsonProcessingException
+import com.fasterxml.jackson.core.type.TypeReference
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import java.io.InputStream
 
 /**
- * 一个简单的Gson序列化与反序列化工具，对gson常用方法做了个包装.
+ * Jackson工具类
+ *
+ * Jackson是一个流行的Json库，这个工具封装了一些简单的方法实现常用的json转换
  * @author Ankol
  */
-public class GsonUtil {
-
-    private static final Gson gson;
-
-    static {
-        gson = new GsonBuilder()
-                .create();
+object JacksonUtil {
+    private val objectMapper = jacksonObjectMapper().apply {
+        //用于确定遇到未知属性时，是否应导致失败（抛出 JsonMappingException）
+        this.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+        this.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false)
     }
 
     /**
-     * 从json字符串反序列化为java对象，此方法支持对带泛型的类进行反序列化操作.
+     * 对象转json
+     * @param obj 对象
+     */
+    fun toJson(obj: Any): String {
+        return objectMapper.writeValueAsString(obj)
+    }
+
+    /**
+     * Json转对象
      * @param json json字符串
-     * @param type 指定的泛型类型，你可以使用{@link com.google.gson.reflect.TypeToken}获取类型。
-     * <p>例如：你需要获取{@code Collection<Foo>}的类型。你可以使用：</p>
-     * <pre>
-     *     new TypeToken&lt;Collection&lt;Foo&gt;&gt;(){};
-     * </pre> 来获取指定泛型类型。
+     * @param target 对象class
      */
-    public static <T> T fromJson(String json, TypeToken<T> type) {
-        return gson.fromJson(json, type.getType());
+    fun <T> fromJson(json: String, target: Class<T>): T {
+        return objectMapper.readValue(json, target)
     }
 
     /**
-     * 从json字符串反序列化为java对象。
+     * Json转对象
+     * @param bytes byte数组
+     * @param target 对象class
+     */
+    fun <T> fromJson(bytes: ByteArray, target: Class<T>): T {
+        return objectMapper.readValue(bytes, target)
+    }
+
+    /**
+     * Json转对象
+     * @param inputStream 输入流
+     * @param target 对象class
+     */
+    fun <T> fromJson(inputStream: InputStream, target: Class<T>): T {
+        return objectMapper.readValue(inputStream, target)
+    }
+
+    /**
+     * Json转对象
+     *
+     * 对于带泛型的对象请使用此方法，使用[TypeReference]传入明确的类型
+     *
+     * Java示例：
+     * ```java
+     * //创建TypeReference
+     * new TypeReference<List<String>>(){}
+     * ```
+     * Kotlin示例：
+     * ```kotlin
+     * //使用更加简单的包装函数
+     * val type = jacksonTypeRef<List<String>>()
+     * ```
      * @param json json字符串
-     * @param target 反序列化的目标对象
-     * @param <T> 所需对象的类型
+     * @param typeReference 类型引用，参考：[TypeReference]
      */
-    public static <T> T fromJson(String json, Class<T> target) {
-        return gson.fromJson(json, target);
+    fun <T> fromJson(json: String, typeReference: TypeReference<T>): T {
+        return objectMapper.readValue(json, typeReference)
     }
 
     /**
-     * 将java对象转换为json字符串
-     * @param obj java对象
+     * 验证json格式是否合法
+     * @param json json字符串
+     * @return [Boolean] json格式是否合法
      */
-    public static String toJson(Object obj) {
-        return gson.toJson(obj);
-    }
-
-    public static String toJson(Object obj, Type typeof) {
-        return gson.toJson(obj, typeof);
-    }
-
-    /**
-     * 判断是否是json字符串
-     * @param str 字符串
-     */
-    public static boolean isJsonStr(String str) {
+    fun validateJson(json: String): Boolean {
         try {
-            JsonParser.parseString(str);
-            return true;
-        } catch (JsonSyntaxException e) {
-            return false;
+            if (json.isNotEmpty()) {
+                objectMapper.readTree(json)
+                return true
+            }
+            return false
+        } catch (e: JsonProcessingException) {
+            return false
         }
+    }
+
+    /**
+     * 获取工具类使用的ObjectMapper
+     * @return [ObjectMapper]
+     */
+    fun getObjectMapper() = objectMapper
+
+    /**
+     * Json转为JsonTree
+     * @return [JsonNode]
+     */
+    fun toTree(json: String): JsonNode {
+        return objectMapper.readTree(json)
+    }
+
+    /**
+     * javaBean转成JsonTree
+     * @return [JsonNode]
+     */
+    fun <T : JsonNode> toTree(obj: Any): T {
+        return objectMapper.valueToTree(obj)
     }
 }
