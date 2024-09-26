5250e45dbe2ccf5b72d85bf8505b48110ef3dcf9
lichengkun <lichengkun@csrd.cn>
2024-09-26 15:59:34 +0800

    :recycle: 代码重构

--- server/src/main/java/org/paratranz/bot/api/entity/data/Application.java
+++ server/src/main/java/org/paratranz/bot/api/entity/data/Application.java
@@ -1,6 +1,5 @@
 package org.paratranz.bot.api.entity.data;
 
-import com.google.gson.annotations.SerializedName;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -11,83 +10,83 @@ import lombok.NoArgsConstructor;
 @Data
 public class Application {
 
-    @SerializedName("id")
+
     private Integer id;
     /**
      * 创建时间
      */
-    @SerializedName("createdAt")
+    
     private String createdAt;
-    @SerializedName("uid")
+    
     private Integer uid;
     /**
      * 申请的项目ID
      */
-    @SerializedName("project")
+    
     private Integer project;
-    @SerializedName("permission")
+    
     private Integer permission;
     /**
      * 申请内容
      */
-    @SerializedName("content")
+    
     private String content;
-    @SerializedName("status")
+    
     private Integer status;
-    @SerializedName("operator")
+    
     private OperatorDTO operator;
-    @SerializedName("reason")
+    
     private String reason;
-    @SerializedName("detail")
+    
     private DetailDTO detail;
-    @SerializedName("tests")
+    
     private Integer tests;
     /**
      * 用户相关信息
      */
-    @SerializedName("user")
+    
     private UserDTO user;
 
     @NoArgsConstructor
     @Data
     public static class OperatorDTO {
-        @SerializedName("id")
+        
         private Integer id;
-        @SerializedName("username")
+        
         private String username;
-        @SerializedName("nickname")
+        
         private String nickname;
-        @SerializedName("avatar")
+        
         private String avatar;
-        @SerializedName("lastVisit")
+        
         private String lastVisit;
     }
 
     @NoArgsConstructor
     @Data
     public static class DetailDTO {
-        @SerializedName("english")
+        
         private EnglishDTO english;
-        @SerializedName("gameTime")
+        
         private Integer gameTime;
-        @SerializedName("experience")
+        
         private ExperienceDTO experience;
 
         @NoArgsConstructor
         @Data
         public static class EnglishDTO {
-            @SerializedName("level")
+            
             private String level;
-            @SerializedName("detail")
+            
             private String detail;
         }
 
         @NoArgsConstructor
         @Data
         public static class ExperienceDTO {
-            @SerializedName("times")
+            
             private Integer times;
-            @SerializedName("detail")
+            
             private String detail;
         }
     }
@@ -95,13 +94,13 @@ public class Application {
     @NoArgsConstructor
     @Data
     public static class UserDTO {
-        @SerializedName("id")
+        
         private Integer id;
-        @SerializedName("username")
+        
         private String username;
-        @SerializedName("avatar")
+        
         private String avatar;
-        @SerializedName("lastVisit")
+        
         private String lastVisit;
     }
 }
--- server/src/main/java/org/paratranz/bot/api/entity/data/ProjectOverview.java
+++ server/src/main/java/org/paratranz/bot/api/entity/data/ProjectOverview.java
@@ -1,6 +1,5 @@
 package org.paratranz.bot.api.entity.data;
 
-import com.google.gson.annotations.SerializedName;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -14,165 +13,165 @@ import java.util.List;
 @NoArgsConstructor
 public class ProjectOverview {
 
-    @SerializedName("id")
+    
     private Integer id;
-    @SerializedName("createdAt")
+    
     private String createdAt;
-    @SerializedName("updatedAt")
+    
     private String updatedAt;
-    @SerializedName("uid")
+    
     private Integer uid;
-    @SerializedName("name")
+    
     private String name;
-    @SerializedName("logo")
+    
     private String logo;
-    @SerializedName("desc")
+    
     private String desc;
-    @SerializedName("source")
+    
     private String source;
-    @SerializedName("dest")
+    
     private String dest;
-    @SerializedName("members")
+    
     private Integer members;
-    @SerializedName("game")
+    
     private String game;
-    @SerializedName("license")
+    
     private String license;
-    @SerializedName("activeLevel")
+    
     private Double activeLevel;
-    @SerializedName("rank")
+    
     private Integer rank;
-    @SerializedName("privacy")
+    
     private Integer privacy;
-    @SerializedName("download")
+    
     private Integer download;
-    @SerializedName("joinMode")
+    
     private Integer joinMode;
-    @SerializedName("issueMode")
+    
     private Integer issueMode;
-    @SerializedName("reviewMode")
+    
     private Integer reviewMode;
-    @SerializedName("stage")
+    
     private Integer stage;
-    @SerializedName("extra")
+    
     private ExtraDTO extra;
-    @SerializedName("owner")
+    
     private OwnerDTO owner;
-    @SerializedName("stats")
+    
     private StatsDTO stats;
-    @SerializedName("relatedGames")
+    
     private List<String> relatedGames;
-    @SerializedName("isPrivate")
+    
     private Boolean isPrivate;
-    @SerializedName("gameName")
+    
     private String gameName;
-    @SerializedName("formats")
+    
     private FormatsDTO formats;
-    @SerializedName("webhook")
+    
     private WebhookDTO webhook;
-    @SerializedName("announcementsCount")
+    
     private Integer announcementsCount;
-    @SerializedName("tasksCount")
+    
     private Integer tasksCount;
-    @SerializedName("filesCount")
+    
     private Integer filesCount;
-    @SerializedName("termsCount")
+    
     private Integer termsCount;
-    @SerializedName("issuesCount")
+    
     private Integer issuesCount;
-    @SerializedName("applicationsCount")
+    
     private Integer applicationsCount;
-    @SerializedName("tags")
+    
     private List<List<String>> tags;
 
     @NoArgsConstructor
     @Data
     public static class ExtraDTO {
-        @SerializedName("link")
+        
         private String link;
-        @SerializedName("task")
+        
         private Boolean task;
-        @SerializedName("isMod")
+        
         private Boolean isMod;
-        @SerializedName("credit")
+        
         private String credit;
-        @SerializedName("version")
+        
         private String version;
-        @SerializedName("testCount")
+        
         private Integer testCount;
-        @SerializedName("compatible")
+        
         private String compatible;
-        @SerializedName("creditLink")
+        
         private String creditLink;
-        @SerializedName("modVersion")
+        
         private String modVersion;
-        @SerializedName("customTests")
+        
         private Boolean customTests;
-        @SerializedName("publishLink")
+        
         private String publishLink;
-        @SerializedName("hasTranslation")
+        
         private Boolean hasTranslation;
     }
 
     @NoArgsConstructor
     @Data
     public static class OwnerDTO {
-        @SerializedName("id")
+        
         private Integer id;
-        @SerializedName("username")
+        
         private String username;
-        @SerializedName("nickname")
+        
         private String nickname;
-        @SerializedName("avatar")
+        
         private String avatar;
-        @SerializedName("lastVisit")
+        
         private String lastVisit;
     }
 
     @NoArgsConstructor
     @Data
     public static class StatsDTO {
-        @SerializedName("id")
+        
         private Integer id;
-        @SerializedName("total")
+        
         private Integer total;
-        @SerializedName("translated")
+        
         private Integer translated;
-        @SerializedName("disputed")
+        
         private Integer disputed;
-        @SerializedName("checked")
+        
         private Integer checked;
-        @SerializedName("reviewed")
+        
         private Integer reviewed;
-        @SerializedName("hidden")
+        
         private Integer hidden;
-        @SerializedName("locked")
+        
         private Integer locked;
-        @SerializedName("words")
+        
         private Integer words;
-        @SerializedName("members")
+        
         private Integer members;
-        @SerializedName("tp")
+        
         private Double tp;
-        @SerializedName("cp")
+        
         private Double cp;
-        @SerializedName("rp")
+        
         private Double rp;
     }
 
     @NoArgsConstructor
     @Data
     public static class FormatsDTO {
-        @SerializedName("yml")
+        
         private String yml;
-        @SerializedName("txt")
+        
         private String txt;
     }
 
     @NoArgsConstructor
     @Data
     public static class WebhookDTO {
-        @SerializedName("discord")
+        
         private String discord;
     }
 }
