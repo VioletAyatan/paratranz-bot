f5d352fe640e4788ffc4169a5b06ad4f4c8db7e5
lichengkun <lichengkun@csrd.cn>
2024-10-31 15:20:02 +0800

    commit

--- server/src/main/java/org/paratranz/bot/api/constant/ApplyStatus.kt
+++ server/src/main/java/org/paratranz/bot/api/constant/ApplyStatus.kt
@@ -1,25 +1,18 @@
-package org.paratranz.bot.api.constant;
+package org.paratranz.bot.api.constant
 
-import lombok.Getter;
-
-@Getter
-public enum ApplyStatus {
+enum class ApplyStatus(val code: Int) {
     /**
      * 待审核
      */
     UN_CONFIRM(0),
+
     /**
      * 已拒绝
      */
     REJECTED(1),
+
     /**
      * 已通过
      */
     ACCEPTED(2),
-    ;
-    private final int code;
-
-    ApplyStatus(int code) {
-        this.code = code;
-    }
 }
